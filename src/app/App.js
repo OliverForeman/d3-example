a3c5d4533a3778965eeff36174dc006b306129d8
OliverForeman <oliverforeman@btinternet.com>
2019-09-10 15:10:14 +0100

    Added controls for selecting graph type

--- src/app/App.js
+++ src/app/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import BarChartController from '../controllers/BarChartController';
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState('bar-chart');
 
   return (
-    <div className="app-container">
-      <BarChartController />
-    </div>
+    <>
+      <div className="display-container">
+        {selectedOption === 'bar-chart' && <BarChartController />}
+        {selectedOption === 'line-graph'}
+      </div>
+      <div className="graph-control">
+        <button type="button" value="bar-chart" onClick={e => setSelectedOption(e.target.value)}>Bar Chart</button>
+        <button type="button" value="line-graph" onClick={e => setSelectedOption(e.target.value)}>Line Graph</button>
+      </div>
+    </>
   );
 };
 
