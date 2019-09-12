968dada8b54c990893d3e936df4d96f30f376c93
OliverForeman <oliverforeman@btinternet.com>
2019-09-12 14:49:35 +0100

    Updates

---
+++ precache-manifest.a4a8f408ec141f78a809051cfd596b45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f72a285b857149cd16f0ea35598ac1",
+    "url": "/d3-example/index.html"
+  },
+  {
+    "revision": "a5df6a859eb4abfead17",
+    "url": "/d3-example/static/css/main.75f69592.chunk.css"
+  },
+  {
+    "revision": "a776b8d1803d26baf814",
+    "url": "/d3-example/static/js/2.77a37227.chunk.js"
+  },
+  {
+    "revision": "a5df6a859eb4abfead17",
+    "url": "/d3-example/static/js/main.38446b6e.chunk.js"
+  },
+  {
+    "revision": "ea9b8c63508d29df3ee1",
+    "url": "/d3-example/static/js/runtime~main.3ba35218.js"
+  }
+]);
